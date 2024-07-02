99505ee9c3729b9e2526b203e101491e8b064899
Jack <barretzp@gmail.com>
2024-07-02 18:19:49 +0400

    i18n: update language

--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,8 @@ export default function Home() {
       <div className="bg-white py-16 sm:py-24 lg:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            <h2 className="inline sm:block">想要获取IP地址信息？</h2>{' '}
-            <p className="inline sm:block">请输入您的IP。</p>
+            <h2 className="inline sm:block">Would you like to obtain IP address information?</h2>
+            <p className="inline sm:block">Please enter your IP.</p>
           </div>
           <form className="mt-10 max-w-md" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex gap-x-4">
@@ -58,7 +58,7 @@ export default function Home() {
                 type="submit"
                 className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                获取
+                Get
               </button>
             </div>
             {errors.ip?.message && <p>{errors.ip?.message}</p>}
